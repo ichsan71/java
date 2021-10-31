5f56b146cff0870ca8fa5910567af82f4a769997
Muhammad Ichsan Kamil <58933009+ichsan71@users.noreply.github.com>
2021-10-31 21:07:08 +0700

    upload belajar java

---
+++ belajarJava/src/com/tutorial/test.java
@@ -0,0 +1,15 @@
+package com.tutorial;
+
+
+class Mahasiswa {
+
+    String nama;
+}
+
+public class test {
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa();
+
+        System.out.println(mhs1.nama = "ajg");
+    }
+}
